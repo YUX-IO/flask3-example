c1e0de70dcab0f44962015a39fd6be2534da472a
YUX <yu.xiao.fr@gmail.com>
2016-05-30 15:34:08 +0200

    init

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, abort, redirect, Response
 import json
+import netease
 from werkzeug.contrib.cache import SimpleCache
 cache = SimpleCache()
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
